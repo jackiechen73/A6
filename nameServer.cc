086495ee3f3ff148a3d7aa0a4e971e5a5bf0b26b
Linda Zheng <thelindazheng@gmail.com>
2021-12-04 18:11:46 -0500

    Add nameserver

--- nameServer.cc
+++ nameServer.cc
@@ -1,1 +1,40 @@
 #include "NameServer.h"
+
+NameServer::NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents )
+: printer(prt), numVendingMachines(numVendingMachines), numStudents(numStudents), 
+studentAssignments( new unsigned int[numStudents]), machineList(new VendingMachine*[numVendingMachines]) {
+    for (unsigned int i = 0; i < numStudents; i += 1) {
+        studentAssignments[i] = i % numVendingMachines;
+    } // for
+}
+
+void NameServer::main() {
+    printer.print(Printer::NameServer, 'S');
+    for (int i == 0 ; i < numVendingMachines; i += 1 ) {
+        _Accept(VMregister);
+        VMidx += 1;
+    } // for
+    for (;;) {
+        _Accept(~NameServer) {
+            break;
+        } or _Accept(getMachine) { 
+            printer.print(Printer::NameServer, 'N', studId, studentAssignments[studId]);
+            studentAssignments[studId] = (studentAssignments[studId] + 1) % numVendingMachines;
+        } or _Accept(getMachineList) {}
+    } // for
+    printer.print(Printer::NameServer, 'F');
+}
+
+void NameServer::VMregister( VendingMachine * vendingmachine ) {
+    vendingMachines[VMidx] = vendingmachine;
+    printer.print(Printer::NameServer, 'R', vendingmachine->getId());
+}
+
+VendingMachine * NameServer::getMachine( unsigned int id ) {
+    studId = id;
+    return vendingMachines[studentAssignments[id]];
+}
+
+VendingMachine ** NameServer::getMachineList() {
+    return machineList;
+}
--- nameServer.h
+++ nameServer.h
@@ -3,6 +3,13 @@
 #include "vendingMachine.h"
 
 _Task NameServer {
+    Printer& printer;
+    unsigned int numVendingMachines;
+    unsigned int numStudents;
+    unsigned int* studentAssignments;
+    unsigned int VMidx = 0;
+    unsigned int studId = 0;
+    VendingMachine** machineList;
 	void main();
   public:
 	NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
